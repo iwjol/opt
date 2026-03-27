eb8e3c43f1ca3df378b3715102cf6a36f6fde7b9
iwjol <j@apple-utm.arm.slackware.com>
2026-03-27 19:14:40 +0000

    4

--- builds/dwm/config.h
+++ builds/dwm/config.h
@@ -112,6 +112,9 @@ static const Key keys[] = {
         { MODKEY|ShiftMask,             XK_s,      changefocusopacity,   {.f = -0.025}},	/* λ opacity */
         { MODKEY|ShiftMask,             XK_z,      changeunfocusopacity, {.f = +0.025}},	/* λ opacity */
         { MODKEY|ShiftMask,             XK_x,      changeunfocusopacity, {.f = -0.025}},	/* λ opacity */
+
+	{ MODKEY,            	 	XK_w,      killclient,     {0} },			/* λ mac */
+
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
