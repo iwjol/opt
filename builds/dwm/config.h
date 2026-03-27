c3f5836185d22a81c3aa037b051aea857dee11c2
iwjol <j@apple-utm.arm.slackware.com>
2026-03-27 17:57:17 +0000

    third

--- builds/dwm/config.h
+++ builds/dwm/config.h
@@ -51,9 +51,9 @@ static const int refreshrate = 180;  /* refresh rate (per second) for client mov
 
 static const Layout layouts[] = {
         /* symbol     arrange function */
-        { "  λ",      tile },    /* first entry is default */
-        { " 󱂬 λ",      NULL },    /* no layout function means floating behavior */
-        { "  λ",      monocle },
+        { "  ",      tile },    /* first entry is default */
+        { " 󱂬 ",      NULL },    /* no layout function means floating behavior */
+        { "  ",      monocle },
 };
 
 /* key definitions */
